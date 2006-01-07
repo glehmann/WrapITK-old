840565509ffc3a9ecd22ac45b43c9aa473069a5b
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-07 16:59:38 +0100

    add required patchs for build
    darcs-hash:20060107155938-2fc9d-dedfdf8fbc0629ba271a99aea8f8b3ccb1aefb78.gz

---
+++ patch/required/itkSpatialObject.h
@@ -0,0 +1,32 @@
+*** itk/Insight/Code/SpatialObject/itkSpatialObject.h	2006-01-07 15:10:17.000000000 +0100
+--- itk-mima2/Insight/Code/SpatialObject/itkSpatialObject.h	2005-10-24 13:51:34.000000000 +0200
+***************
+*** 394,400 ****
+  
+    /** Return a raw pointer to the node container */
+    typename TreeNodeType::Pointer GetTreeNode() {return m_TreeNode;}
+!   typename TreeNodeType::ConstPointer GetTreeNode() const {return m_TreeNode;}
+  
+    /** Theses functions are just calling the AffineGeometryFrame functions */
+    /** Set the spacing of the spatial object. */
+--- 394,400 ----
+  
+    /** Return a raw pointer to the node container */
+    typename TreeNodeType::Pointer GetTreeNode() {return m_TreeNode;}
+!   //typename TreeNodeType::ConstPointer GetTreeNode() const {return m_TreeNode;}
+  
+    /** Theses functions are just calling the AffineGeometryFrame functions */
+    /** Set the spacing of the spatial object. */
+***************
+*** 597,602 ****
+--- 597,606 ----
+  
+    /** Default outside value for the ValueAt() */
+    double m_DefaultOutsideValue;
++ 
++   SpatialObject(const Self&); //purposely not implemented
++   void operator=(const Self&); //purposely not implemented
++    
+  }; 
+  
+  } // end of namespace itk

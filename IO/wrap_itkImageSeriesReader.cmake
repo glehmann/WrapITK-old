903dface6189926d9883b596851b87ff0722d479
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-12 23:26:09 +0100

    wrap more types
    darcs-hash:20060112222609-2fc9d-e78c4b1194a95a2d0502f76894f4810f53efd8b8.gz

--- IO/wrap_itkImageSeriesReader.cmake
+++ IO/wrap_itkImageSeriesReader.cmake
@@ -1,12 +1,15 @@
 WRAP_CLASS("ImageSeriesReader" POINTER)
+
   FOREACH(d ${WRAP_DIMS})
-    COND_WRAP("${ITKM_IF${d}}"  "${ITKT_IF${d}}"  "F")
-    COND_WRAP("${ITKM_ID${d}}"  "${ITKT_ID${d}}"  "D")
     COND_WRAP("${ITKM_IUC${d}}" "${ITKT_IUC${d}}" "")  # neded to save in 8 bits
     COND_WRAP("${ITKM_IUS${d}}" "${ITKT_IUS${d}}" "US")
     COND_WRAP("${ITKM_IUL${d}}" "${ITKT_IUL${d}}" "UL")
-    COND_WRAP("${ITKM_ISC${d}}" "${ITKT_ISC${d}}" "SC")
-    COND_WRAP("${ITKM_ISS${d}}" "${ITKT_ISS${d}}" "SS")
-    COND_WRAP("${ITKM_ISL${d}}" "${ITKT_ISL${d}}" "SL")
   ENDFOREACH(d)
+
+  WRAP_SIGN_INT(1)
+  WRAP_REAL(1)
+  WRAP_VECTOR_REAL(1)
+  WRAP_COV_VECTOR_REAL(1)
+  WRAP_RGB(1)
+
 END_WRAP_CLASS()

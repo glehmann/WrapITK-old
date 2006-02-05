08a777b69573eca61e5fed79b8ae758051e1e373
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-02-06 00:28:07 +0100

    enhance type prefix definition
    darcs-hash:20060205232807-2fc9d-6e78136c6282050c404a92bd06b652e1b1e7f977.gz

--- WrapTypePrefix.cmake
+++ WrapTypePrefix.cmake
@@ -1,31 +1,36 @@
 #------------------------------------------------------------------------------
-MACRO(CREATE_WRAP_TYPE CLASS PREFIX WRAP)
-   # Create wrapping types : ITKT_Xxx & ITKM_Xxx
-   FOREACH(wrap ${WRAP})
-      STRING(REGEX REPLACE
-         "([0-9A-Za-z]*)[ ]*#[ ]*(.*)"
-         "\\1"
-         wrapTpl "${wrap}"
-      )
-      STRING(REGEX REPLACE
-         "([0-9A-Za-z]*)[ ]*#[ ]*(.*)"
-         "\\2"
-         wrapType "${wrap}"
-      )
-      SET(ITKT_${PREFIX}${wrapTpl} "itk::${CLASS}< ${wrapType} >")
-      SET(ITKM_${PREFIX}${wrapTpl} "${PREFIX}${wrapTpl}")
-   ENDFOREACH(wrap ${WRAP})
-ENDMACRO(CREATE_WRAP_TYPE)
+
+# define some macro to help creation of types vars
 
 MACRO(WRAP_TYPE class prefix)
+   # begin the creation of a type vars
+   # call to this macro should be followed by one or several call to WRAP()
+   # and one call to END_WRAP_TYPE to really create the vars
    SET(WRAPPER_TEMPLATES "")
    SET(itk_Wrap_Prefix "${prefix}")
    SET(itk_Wrap_Class "${class}")
 ENDMACRO(WRAP_TYPE)
 
 MACRO(END_WRAP_TYPE)
-   CREATE_WRAP_TYPE("${itk_Wrap_Class}" "${itk_Wrap_Prefix}" "${WRAPPER_TEMPLATES}")
+   # create the type vars.
+   # must be called after END_WRAP_TYPE
+   # Create the vars used for to designe types in all the cmake
+   # files. This method ensure all the type names are constructed
+   # with the same method
+   FOREACH(wrap ${WRAPPER_TEMPLATES})
+      STRING(REGEX REPLACE "([0-9A-Za-z]*)[ ]*#[ ]*(.*)" "\\1" wrapTpl "${wrap}")
+      STRING(REGEX REPLACE "([0-9A-Za-z]*)[ ]*#[ ]*(.*)" "\\2" wrapType "${wrap}")
+      SET(ITKT_${itk_Wrap_Prefix}${wrapTpl} "itk::${itk_Wrap_Class}< ${wrapType} >")
+      SET(ITKM_${itk_Wrap_Prefix}${wrapTpl} "${itk_Wrap_Prefix}${wrapTpl}")
+   ENDFOREACH(wrap)
 ENDMACRO(END_WRAP_TYPE)
+
+#------------------------------------------------------------------------------
+
+# now, define types vars
+# the result is stored in itk_Wrap_XXX where XXX is the name of the class
+# to be wrapped in there own file, most of the time in CommonA
+
 
 WRAP_TYPE("Offset" "O")
   FOREACH(d ${WRAP_DIMS})
@@ -34,7 +39,6 @@ WRAP_TYPE("Offset" "O")
 END_WRAP_TYPE()
 SET(itk_Wrap_Offset ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
 WRAP_TYPE("Vector" "V")
   FOREACH(d ${WRAP_DIMS})
     WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}")
@@ -43,7 +47,6 @@ WRAP_TYPE("Vector" "V")
 END_WRAP_TYPE()
 SET(itk_Wrap_Vector ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
 WRAP_TYPE("CovariantVector" "CV")
   FOREACH(d ${WRAP_DIMS})
     WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}")
@@ -52,7 +55,6 @@ WRAP_TYPE("CovariantVector" "CV")
 END_WRAP_TYPE()
 SET(itk_Wrap_CovariantVector ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
 WRAP_TYPE("ContinuousIndex" "CI")
   FOREACH(d ${WRAP_DIMS})
     WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}")
@@ -61,61 +63,47 @@ WRAP_TYPE("ContinuousIndex" "CI")
 END_WRAP_TYPE()
 SET(itk_Wrap_ContinuousIndex ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
-SET(WRAP_PREFIX_Array "A")
-SET(itk_Wrap_Array
-   "${ITKM_D}  # ${ITKT_D}"
-   "${ITKM_F}  # ${ITKT_F}"
-)
-CREATE_WRAP_TYPE("Array" ${WRAP_PREFIX_Array} "${itk_Wrap_Array}")
+WRAP_TYPE("Array" "A")
+  WRAP("${ITKM_D}" "${ITKT_D}")
+  WRAP("${ITKM_F}" "${ITKT_F}")
+END_WRAP_TYPE()
+SET(itk_Wrap_Array ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
-SET(WRAP_PREFIX_FixedArray "FA")
-SET(itk_Wrap_FixedArray
-   "${ITKM_D}1  # ${ITKT_D},1"
-   "${ITKM_F}1  # ${ITKT_F},1"
-   "${ITKM_UL}1 # ${ITKT_UL},1"
-   "${ITKM_US}1 # ${ITKT_US},1"
-   "${ITKM_UC}1 # ${ITKT_UC},1"
-   "${ITKM_UI}1 # ${ITKT_UI},1"
-   "${ITKM_SL}1 # ${ITKT_SL},1"
-   "${ITKM_SS}1 # ${ITKT_SS},1"
-   "${ITKM_SC}1 # ${ITKT_SC},1"
-   "${ITKM_B}1 # ${ITKT_B},1"
+WRAP_TYPE("FixedArray" "FA")
+   WRAP("${ITKM_D}1"  "${ITKT_D},1")
+   WRAP("${ITKM_F}1"  "${ITKT_F},1")
+   WRAP("${ITKM_UL}1" "${ITKT_UL},1")
+   WRAP("${ITKM_US}1" "${ITKT_US},1")
+   WRAP("${ITKM_UC}1" "${ITKT_UC},1")
+   WRAP("${ITKM_UI}1" "${ITKT_UI},1")
+   WRAP("${ITKM_SL}1" "${ITKT_SL},1")
+   WRAP("${ITKM_SS}1" "${ITKT_SS},1")
+   WRAP("${ITKM_SC}1" "${ITKT_SC},1")
+   WRAP("${ITKM_B}1"  "${ITKT_B},1")
 
-   "${ITKM_D}2  # ${ITKT_D},2"
-   "${ITKM_F}2  # ${ITKT_F},2"
-   "${ITKM_UL}2 # ${ITKT_UL},2"
-   "${ITKM_US}2 # ${ITKT_US},2"
-   "${ITKM_UC}2 # ${ITKT_UC},2"
-   "${ITKM_UI}2 # ${ITKT_UI},2"
-   "${ITKM_SL}2 # ${ITKT_SL},2"
-   "${ITKM_SS}2 # ${ITKT_SS},2"
-   "${ITKM_SC}2 # ${ITKT_SC},2"
-   "${ITKM_B}2 # ${ITKT_B},2"
+  FOREACH(d ${WRAP_DIMS})
+    IF(NOT "${d}" EQUAL 1)
+      WRAP("${ITKM_D}${d}"  "${ITKT_D},${d}")
+      WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}")
+      WRAP("${ITKM_UL}${d}" "${ITKT_UL},${d}")
+      WRAP("${ITKM_US}${d}" "${ITKT_US},${d}")
+      WRAP("${ITKM_UC}${d}" "${ITKT_UC},${d}")
+      WRAP("${ITKM_UI}${d}" "${ITKT_UI},${d}")
+      WRAP("${ITKM_SL}${d}" "${ITKT_SL},${d}")
+      WRAP("${ITKM_SS}${d}" "${ITKT_SS},${d}")
+      WRAP("${ITKM_SC}${d}" "${ITKT_SC},${d}")
+      WRAP("${ITKM_B}${d}"  "${ITKT_B},${d}")
+    ENDIF(NOT "${d}" EQUAL 1)
+  ENDFOREACH(d)
+END_WRAP_TYPE()
+SET(itk_Wrap_FixedArray ${WRAPPER_TEMPLATES})
 
-   "${ITKM_D}3  # ${ITKT_D},3"
-   "${ITKM_F}3  # ${ITKT_F},3"
-   "${ITKM_UL}3 # ${ITKT_UL},3"
-   "${ITKM_US}3 # ${ITKT_US},3"
-   "${ITKM_UC}3 # ${ITKT_UC},3"
-   "${ITKM_UI}3 # ${ITKT_UI},3"
-   "${ITKM_SL}3 # ${ITKT_SL},3"
-   "${ITKM_SS}3 # ${ITKT_SS},3"
-   "${ITKM_SC}3 # ${ITKT_SC},3"
-   "${ITKM_B}3 # ${ITKT_B},3"
-)
-CREATE_WRAP_TYPE("FixedArray" ${WRAP_PREFIX_FixedArray} "${itk_Wrap_FixedArray}")
-
-#------------------------------------------------------------------------------
 WRAP_TYPE("RGBPixel" "RGB")
   COND_WRAP("${ITKM_UC}" "${ITKT_UC}" "RGBUC")
   COND_WRAP("${ITKM_US}" "${ITKT_US}" "RGBUS")
 END_WRAP_TYPE()
 SET(itk_Wrap_RGBPixel ${WRAPPER_TEMPLATES})
 
-
-#------------------------------------------------------------------------------
 WRAP_TYPE("Image" "I")
   FOREACH(d ${WRAP_DIMS})
     COND_WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}"  "F")
@@ -137,8 +125,6 @@ WRAP_TYPE("Image" "I")
 END_WRAP_TYPE()
 SET(itk_Wrap_Image ${WRAPPER_TEMPLATES})
 
-
-#------------------------------------------------------------------------------
 WRAP_TYPE("Point" "P")
   FOREACH(d ${WRAP_DIMS})
     WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}")
@@ -147,7 +133,6 @@ WRAP_TYPE("Point" "P")
 END_WRAP_TYPE()
 SET(itk_Wrap_Point ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
 WRAP_TYPE("LevelSetNode" "LSN")
   FOREACH(d ${WRAP_DIMS})
     WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}")
@@ -162,8 +147,6 @@ WRAP_TYPE("LevelSetNode" "LSN")
 END_WRAP_TYPE()
 SET(itk_Wrap_LevelSetNode ${WRAPPER_TEMPLATES})
 
-
-#------------------------------------------------------------------------------
 WRAP_TYPE("BinaryBallStructuringElement" "SE")
   FOREACH(d ${WRAP_DIMS})
     COND_WRAP("${ITKM_F}${d}"  "${ITKT_F},${d}"  "F")
@@ -178,12 +161,9 @@ WRAP_TYPE("BinaryBallStructuringElement" "SE")
 END_WRAP_TYPE()
 SET(itk_Wrap_StructuringElement ${WRAPPER_TEMPLATES})
 
-#------------------------------------------------------------------------------
 WRAP_TYPE("SpatialObject" "SO")
   FOREACH(d ${WRAP_DIMS})
     WRAP("${d}"  "${d}")
   ENDFOREACH(d)
 END_WRAP_TYPE()
 SET(itk_Wrap_SpatialObject ${WRAPPER_TEMPLATES})
-
-#------------------------------------------------------------------------------

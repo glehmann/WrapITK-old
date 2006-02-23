4be1960374f343563224e89edd947e38e1f7bcf9
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-02-23 01:51:32 +0100

    use the new macros to create the ImportImageFilter type set
    darcs-hash:20060223005132-2fc9d-346b3093e7b1f1ca94c6711643e21936e0475531.gz

--- BasicFiltersA/wrap_itkImportImageFilter.cmake
+++ BasicFiltersA/wrap_itkImportImageFilter.cmake
@@ -1,14 +1,9 @@
 WRAP_CLASS("itk::ImportImageFilter" POINTER)
 
   FOREACH(d ${WRAP_ITK_DIMS})
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_D}${d}"  "${ITKT_D},${d}"  "D" )
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_F}${d}"  "${ITKT_F},${d}"  "F" )
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_UL}${d}" "${ITKT_UL},${d}" "UL")
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_US}${d}" "${ITKT_US},${d}" "US")
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_UC}${d}" "${ITKT_UC},${d}" "UC")
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_SL}${d}" "${ITKT_SL},${d}" "SL")
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_SS}${d}" "${ITKT_SS},${d}" "SS")
-    WRAP_TEMPLATE_IF_TYPES("${ITKM_SC}${d}" "${ITKT_SC},${d}" "SC")
+    FOREACH(t ${WRAP_ITK_SCALAR})
+      WRAP_TEMPLATE("${ITKM_${t}}${d}" "${ITKT_${t}},${d}")
+    ENDFOREACH(t)
   ENDFOREACH(d)
 
 END_WRAP_CLASS()

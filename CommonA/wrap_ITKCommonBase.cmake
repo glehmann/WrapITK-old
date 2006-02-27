410b452b67cbf0835404e6ede4ba1622e35257a5
Zachary Pincus <zpincus@stanford.edu>
2006-02-27 03:41:41 +0100

    Fix (hopefully) MultiThreader and BoundaryCondition issues with __stdcall and __declspec on windows.
    darcs-hash:20060227024141-733a7-8cd5f4808d4345ccb60b1199962d06ddd0f6523f.gz

--- CommonA/wrap_ITKCommonBase.cmake
+++ CommonA/wrap_ITKCommonBase.cmake
@@ -9,8 +9,17 @@ WRAP_NON_TEMPLATE_CLASS("itk::LightProcessObject" POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::ProcessObject"      POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::OutputWindow"       POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::Version"            POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::MultiThreader"      POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::TimeStamp")
 WRAP_NON_TEMPLATE_CLASS("itk::Indent")
 WRAP_NON_TEMPLATE_CLASS("itk::StringStream")
 WRAP_NON_TEMPLATE_CLASS("itk::MetaDataDictionary")
+
+# gcc-xml on windows cannot be allowed to see the full MultiThreader
+# header because currently gcc-xml and cswig cannot properly wrap functions
+# that use __stdcall specifiers, which one part of MultiThreader.h does on
+# windows. Just pass the stub header which will keep gcc-xml away from the 
+# real header on windows.
+SET(WRAPPER_AUTO_INCLUDE_HEADERS OFF)
+WRAP_INCLUDE("itkMultiThreaderStub.h")
+WRAP_NON_TEMPLATE_CLASS("itk::MultiThreader"      POINTER)
+
--- CommonA/wrap_itkImageBoundaryCondition.cmake
+++ CommonA/wrap_itkImageBoundaryCondition.cmake
@@ -6,15 +6,6 @@
 #   WRAP_IMAGE_FILTER_COV_VECTOR_REAL(1)
 # END_WRAP_CLASS()
 
-WRAP_CLASS("itk::ConstantBoundaryCondition")
-  WRAP_IMAGE_FILTER_INT(1)
-  WRAP_IMAGE_FILTER_SIGN_INT(1)
-  WRAP_IMAGE_FILTER_REAL(1)
-  WRAP_IMAGE_FILTER_VECTOR_REAL(1)
-  WRAP_IMAGE_FILTER_COV_VECTOR_REAL(1)
-  WRAP_IMAGE_FILTER_RGB(1)
-END_WRAP_CLASS()
-
 WRAP_CLASS("itk::PeriodicBoundaryCondition")
   WRAP_IMAGE_FILTER_INT(1)
   WRAP_IMAGE_FILTER_SIGN_INT(1)
@@ -25,6 +16,22 @@ WRAP_CLASS("itk::PeriodicBoundaryCondition")
 END_WRAP_CLASS()
 
 WRAP_CLASS("itk::ZeroFluxNeumannBoundaryCondition")
+  WRAP_IMAGE_FILTER_INT(1)
+  WRAP_IMAGE_FILTER_SIGN_INT(1)
+  WRAP_IMAGE_FILTER_REAL(1)
+  WRAP_IMAGE_FILTER_VECTOR_REAL(1)
+  WRAP_IMAGE_FILTER_COV_VECTOR_REAL(1)
+  WRAP_IMAGE_FILTER_RGB(1)
+END_WRAP_CLASS()
+
+# gcc-xml on windows cannot be allowed to see the full ConstantBoundaryCondition
+# header because currently gcc-xml and cswig cannot properly wrap functions
+# that use __declspec specifiers, which one part of itkConstantBoundaryCondition.h
+# does on windows. Just pass the stub header which will keep gcc-xml away from the 
+# real header on windows.
+SET(WRAPPER_AUTO_INCLUDE_HEADERS OFF)
+WRAP_INCLUDE("itkConstantBoundaryConditionStub.h")
+WRAP_CLASS("itk::ConstantBoundaryCondition")
   WRAP_IMAGE_FILTER_INT(1)
   WRAP_IMAGE_FILTER_SIGN_INT(1)
   WRAP_IMAGE_FILTER_REAL(1)

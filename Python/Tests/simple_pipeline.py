4f06701d19dac9c8175f9e8d768cd5f7ae6b9692
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-01 16:30:41 +0100

    add some filter to test that objects can be passed from one module to one other
    darcs-hash:20060301153041-2fc9d-e4eaf9b025879ead9e292a53db0e5d69cfb489c8.gz

--- Python/Tests/simple_pipeline.py
+++ Python/Tests/simple_pipeline.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+def custom_callback(name):
+  print "loading %s submodule..." % name
+import itkConfig
+itkConfig.ImportCallback = custom_callback
+
 import itk, sys
 
 # first argument is the pixel type
@@ -12,8 +17,16 @@ IType = itk.Image[PType, dim]
 
 # create the reader
 reader = itk.ImageFileReader[IType].New(FileName=sys.argv[3])
+# use a filter outside the IO sub module to be sure objects can be pass
+# from one module to one other
+cast1 = itk.CastImageFilter[IType, IType].New(reader)
+cast1.Update()
+shift = itk.ShiftScaleImageFilter[IType, IType].New(cast1)
+shift.Update()
+cast2 = itk.CastImageFilter[IType, IType].New(shift)
+cast2.Update()
 # and the writer
-writer = itk.ImageFileWriter[IType].New(reader, FileName=sys.argv[4])
+writer = itk.ImageFileWriter[IType].New(cast2, FileName=sys.argv[4])
 
 # execute the filters in the pipeline !
 writer.Update()

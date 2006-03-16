604bb71efea456096e30cac74b5745e6044802ca
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-16 17:05:05 +0100

    filter on dims to avoid wrapping gap when dim 2 or dim 3 is not selected
    darcs-hash:20060316160505-2fc9d-ad4137ececb64a7ba914a27dcee8828f32524180.gz

--- CommonB/wrap_ITKRigidTransforms.cmake
+++ CommonB/wrap_ITKRigidTransforms.cmake
@@ -1,27 +1,36 @@
-WRAP_CLASS("itk::CenteredRigid2DTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()
+FILTER_DIMS(d2 2)
+IF(d2)
+  WRAP_CLASS("itk::CenteredRigid2DTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+  
+  WRAP_CLASS("itk::Euler2DTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+  
+  WRAP_CLASS("itk::Rigid2DTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+ENDIF(d2)
 
-WRAP_CLASS("itk::Euler2DTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()
 
-WRAP_CLASS("itk::QuaternionRigidTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()
 
-WRAP_CLASS("itk::Rigid2DTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()
+FILTER_DIMS(d3 3)
+IF(d3)
+  WRAP_CLASS("itk::Rigid3DPerspectiveTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+  
+  WRAP_CLASS("itk::Rigid3DTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+  
+  WRAP_CLASS("itk::VersorRigid3DTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+  
+  WRAP_CLASS("itk::QuaternionRigidTransform" POINTER)
+    WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
+  END_WRAP_CLASS()
+ENDIF(d3)
 
-WRAP_CLASS("itk::Rigid3DPerspectiveTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()
-
-WRAP_CLASS("itk::Rigid3DTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()
-
-WRAP_CLASS("itk::VersorRigid3DTransform" POINTER)
-  WRAP_TEMPLATE("${ITKM_D}" "${ITKT_D}")
-END_WRAP_CLASS()

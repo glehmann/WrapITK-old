294302db15a2979a206c48f8737481232b76fdc4
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-31 00:20:09 +0200

    disable DiscreteGaussianImageFilter and add a comment
    darcs-hash:20060330222009-2fc9d-48700bbdd54db1f78357387de2eb558c64a9b84a.gz

--- BasicFiltersC/wrap_itkDiscreteGaussianImageFilter.cmake
+++ BasicFiltersC/wrap_itkDiscreteGaussianImageFilter.cmake
@@ -1,5 +1,9 @@
-WRAP_CLASS("itk::DiscreteGaussianImageFilter" POINTER)
-  WRAP_IMAGE_FILTER_INT(2)
-  WRAP_IMAGE_FILTER_SIGN_INT(2)
-  WRAP_IMAGE_FILTER_REAL(2)
-END_WRAP_CLASS()
+# swig generate bad code for python with that filter for an unknown
+# it seems that it is a bad interaction with FixedArray typemap
+# we can't enable it for now
+
+# WRAP_CLASS("itk::DiscreteGaussianImageFilter" POINTER)
+#   WRAP_IMAGE_FILTER_INT(2)
+#   WRAP_IMAGE_FILTER_SIGN_INT(2)
+#   WRAP_IMAGE_FILTER_REAL(2)
+# END_WRAP_CLASS()

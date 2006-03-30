f58f814243aea6c553456b3233fa18014d09bab3
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-30 23:54:21 +0200

    resurect VoronoiSegmentationImageFilter wrapper and adapt required classes wrappers
    darcs-hash:20060330215421-2fc9d-20415c883b4693899439654589949039ec95b381.gz

--- CommonA/wrap_itkMesh.cmake
+++ CommonA/wrap_itkMesh.cmake
@@ -1,7 +1,7 @@
 WRAP_INCLUDE("itkDefaultStaticMeshTraits.h")
 WRAP_INCLUDE("itkDefaultDynamicMeshTraits.h")
 
-WRAP_CLASS("itk::PointSet" POINTER)
+WRAP_CLASS("itk::Mesh" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
     WRAP_TEMPLATE("${ITKM_D}${d}S"
       "${ITKT_D},${d},itk::DefaultStaticMeshTraits< ${ITKT_D},${d},${d},${ITKT_D} >")

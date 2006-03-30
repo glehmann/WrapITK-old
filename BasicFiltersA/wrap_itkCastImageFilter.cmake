99366de4882e8fea102c14cc7204f397885e5164
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-30 15:17:50 +0200

    add missing type
    darcs-hash:20060330131750-2fc9d-563b94625e77ff2c8c7884f603fe8bc066bdbe6f.gz

--- BasicFiltersA/wrap_itkCastImageFilter.cmake
+++ BasicFiltersA/wrap_itkCastImageFilter.cmake
@@ -1,7 +1,7 @@
 WRAP_CLASS("itk::CastImageFilter" POINTER_WITH_SUPERCLASS)
   # Create cast filters between all scalar types. Also force that cast-to-uchar
   # filters are created for all scalar types.
-  UNIQUE(to_types "${WRAP_ITK_SCALAR};UC")
-  WRAP_IMAGE_FILTER_COMBINATIONS("${WRAP_ITK_SCALAR}" "${to_types}")
+  UNIQUE(types "${WRAP_ITK_SCALAR};UC")
+  WRAP_IMAGE_FILTER_COMBINATIONS("${types}" "${types}")
 END_WRAP_CLASS()
 

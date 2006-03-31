4dad0d2eae7d692be1b1c90f02dc683e505f38aa
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-03-31 09:57:37 +0200

    disable CannyEdgeDetectionImageFilter wrapper: it is broken with python
    darcs-hash:20060331075737-2fc9d-8cd94f9af646769992274dbdacb7a7f09be4131e.gz

--- BasicFiltersA/wrap_itkCannyEdgeDetectionImageFilter.cmake
+++ BasicFiltersA/wrap_itkCannyEdgeDetectionImageFilter.cmake
@@ -1,4 +1,7 @@
-WRAP_CLASS("itk::CannyEdgeDetectionImageFilter" POINTER)
-  WRAP_IMAGE_FILTER_REAL(2 2+)
-END_WRAP_CLASS()
+# again that problem with python and FixedArray typemap
+# this class can't be used
+
+# WRAP_CLASS("itk::CannyEdgeDetectionImageFilter" POINTER)
+#  WRAP_IMAGE_FILTER_REAL(2 2+)
+# END_WRAP_CLASS()
 

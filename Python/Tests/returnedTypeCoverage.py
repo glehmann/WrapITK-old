e8892c74a54b88c73d36396957f62c9c4fabbbaf
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-04-02 13:38:14 +0200

    enhance returnedTypeCoverage.py
    darcs-hash:20060402113814-2fc9d-a0e7eba3f3d551a092270287ac32265b51d3f46c.gz

--- Python/Tests/returnedTypeCoverage.py
+++ Python/Tests/returnedTypeCoverage.py
@@ -1,45 +1,190 @@
 import itk, re, sys
-itk.auto_progress = True
 try:
     import itkvtk
 except:
     pass
 from itkTemplate import itkTemplate
+from optparse import OptionParser
+
+
+ctypes = [
+  'double',
+  'float',
+  'signed_char',
+  'signed_short',
+  'signed_long',
+  'unsigned_char',
+  'unsigned_short',
+  'unsigned_long',
+  'char',
+  'short',
+  'long',
+  'bool',
+  'int',
+  'unsigned_int',
+  'void'
+]
+
+
+excludedMethodsList = [
+  'Delete',
+  'Unregister', 
+  'SetReferenceCount',
+  
+  # the method broken for all filters
+  'PushBackInput',
+  'PushFrontInput',
+  'GraftOutput',
+  'SetInput',
+  'UpdateOutputData',
+  'PropagateRequestedRegion',
+  'EnlargeOutputRequestedRegion',
+  
+  # functor are not wrapped so exclude GetFunctor
+  'GetFunctor',
+  
+  #'UnCreateAllInstanceRegister',
+  #'Update',
+  #'UpdateLargestPossibleRegion',
+  #'GenerateInputRequestedRegion',
+  #'GenerateOutputInformation',
+  #'GenerateData',
+  #'GetImageIO', 
+  #'UpdateOutputData',
+  #'UpdateOutputInformation',
+  #'GraftOutput',
+  #'AddSeed',
+  #'GetInputs',
+  #'GetOutputs',
+  #'CreateAllInstance'
+]
+
+excludedMethodsWithParamList = [
+  #'GetElement',
+  #'RegisterFactory',
+  #'UnRegisterFactory',
+  #'PropagateRequestedRegion',
+  #'Resize',
+  #'PadByRadius'
+]
+
+excludedClasses = [
+  #'BinaryBallStructuringElement',
+  #'Neighborhood',
+  #'BSplineDecompositionImageFilter',
+  #'AtanImageFilter',
+  #'Atan2ImageFilter',
+  #'ChangeInformationImageFilter',
+  #'GeodesicActiveContourLevelSetImageFilter',
+  #'Image',
+  #'ImageBase',
+  #'ImageFileReader',
+  #'ImageFileWriter',
+  #'ImageRegistrationMethod',
+  #'BSplineDownsampleImageFilter',
+  #'BSplineUpsampleImageFilter',
+  #'CropImageFilter',
+  #'LevelSetFunction',
+  #'MattesMutualInformationImageToImageMetric',
+  #'MeanReciprocalSquareDifferenceImageToImageMetric',
+  #'MeanSquaresImageToImageMetric',
+  #'MultiResolutionImageRegistrationMethod',
+  #'MultiResolutionPyramidImageFilter',
+  #'MutualInformationImageToImageMetric',
+  #'NormalizedCorrelationImageToImageMetric',
+  #'ParallelSparseFieldLevelSetImageFilter',
+  #'PyBuffer',
+  #'RawImageIO',
+  #'RecursiveMultiResolutionPyramidImageFilter',
+  #'ResampleImageFilter',
+  #'RescaleIntensityImageFilter',
+  #'SegmentationLevelSetImageFilter',
+  #'ShapeDetectionLevelSetImageFilter',
+  #'SparseFieldFourthOrderLevelSetImageFilter',
+  #'SparseFieldLevelSetImageFilter',
+  #'SpatialObject',
+  #'SpatialObjectTreeNode',
+  #'WatershedImageFilter',
+  #'TreeNode',
+  #'ThresholdSegmentationLevelSetImageFilter',
+  #'PointSet',
+  #'ImageConstIterator',
+  #'ImageConstIteratorWithIndex',
+  #'ImageIterator',
+  #'ImageIteratorWithIndex',
+  #'ImageLinearConstIteratorWithIndex',
+  #'ImageLinearIteratorWithIndex',
+  #'ImageRandomConstIteratorWithIndex',
+  #'ImageRandomIteratorWithIndex',
+  #'ImageRandomNonRepeatingConstIteratorWithIndex',
+  #'ImageRandomNonRepeatingIteratorWithIndex',
+  #'ImageRegionConstIterator',
+  #'ImageRegionIterator',
+  #'ImageRegionConstIteratorWithIndex',
+  #'ImageRegionIteratorWithIndex',
+  #'InterpolateImageFilter',
+  #'IsolatedConnectedImageFilter',
+  'SmartPointer'
+]
+
+def log(s, level):
+  if level <= options.verbose:
+    print s
+    sys.stdout.flush()
+
+
+def cleanType(s):
+  i = s.index('_', 1)
+  return s[i+1:]
+
+
+def addUnwrappedType(s):
+  s = cleanType(s)
+  if not s in unwrappedTypes:
+    unwrappedTypes.add(s)
+    log(s, 0)
 
 
 def exploreTpl(tpl):
     for cl in tpl.itervalues():
-	exploreMethods(cl)
+	exploreMethods(cl, cl)
 	# try to instanciate the class
 	try :
 	    obj = cl.New()
-	    exploreMethods(obj)
+	    exploreMethods(obj, cl)
 	except:
 	    pass
 	try :
-	    exploreMethods(cl())
+	    exploreMethods(cl(), cl)
 	except:
 	    pass
     
-def exploreMethods(obj):
-    excludeList = ['Delete', 'Unregister', 'UnCreateAllInstanceRegister', 'Update', 'UpdateLargestPossibleRegion', 'GenerateInputRequestedRegion', 'GenerateOutputInformation', 'GenerateData', 'GetImageIO', 'EnlargeOutputRequestedRegion', 'UpdateOutputData', 'UpdateOutputInformation', 'GraftOutput', 'AddSeed', 'GetInputs', 'GetOutputs', 'GetFunctor', 'CreateAllInstance']
-    attrNameList = [i for i in dir(obj) if isinstance(i, str) and i[0].isupper() and i not in excludeList]
+def exploreMethods(obj, cl):
+    attrNameList = [i for i in dir(obj) if isinstance(i, str) and i[0].isupper() and i not in excludedMethodsList]
 	    
     for attrName in attrNameList:
-# 	print "   +++", attrName; sys.stdout.flush()
+	log(" + " + attrName, 2)
 	try :
-	    exec "s = obj.%s()" % attrName
-	    if isUnwrappedTypeString(s):
-		print repr(s), ":", attrName #, ":" , repr(obj)
+            parameters = repr((cl.__name__, attrName))
+            if parameters not in exclude :
+              log(parameters, 4)
+              exec "s = obj.%s()" % attrName
+              if isUnwrappedTypeString(s):
+                  addUnwrappedType(s)
+                  log("   - " + cleanType(s), 5)
 	except :
 	    # try with some parameters
-	    if attrName not in ['GetElement', 'RegisterFactory', 'UnRegisterFactory', 'PropagateRequestedRegion', 'Resize', 'PadByRadius'] and not attrName.startswith('Set') :
-#  		print '   ***', attrName; sys.stdout.flush()
+	    if attrName not in excludedMethodsWithParamList :
 		for param in [0, '', False, None] :
+                  parameters = repr((cl.__name__, attrName, param))
+                  if parameters not in exclude :
+		    log('  * ' + repr(param), 3)
+                    log(parameters, 4)
 		    try :
 			exec "s = obj.%s(param)" % attrName
 			if isUnwrappedTypeString(s):
-			    print repr(s), ":", attrName #, ":", repr(obj)
+                          addUnwrappedType(s)
+                          log("   - " + cleanType(s), 5)
 		    except :
 			pass
 	
@@ -48,85 +193,45 @@ def isUnwrappedTypeString(s):
 	return False
     if not s[0] == "_":
 	return False
-    for t in ['double', 'float', 'signed_char', 'signed_short', 'signed_long', 'unsigned_char', 'unsigned_short', 'unsigned_long', 'char', 'short', 'long', 'bool', 'int', 'unsigned_int', 'void'] :
+    for t in ctypes :
 	if re.match('^_[0-9a-z]+_p_%s$' % t, s) :
 	    return False
     return True
     
-attrNameList = [i for i in dir(itk) if i[0].isupper() and len(i) > 2]
 
-# remove attr which cause problems
-attrNameList.remove('BinaryBallStructuringElement')
-attrNameList.remove('Neighborhood')
-# attrNameList.remove('BSplineDecompositionImageFilter')
-# attrNameList.remove('AtanImageFilter')
-# attrNameList.remove('Atan2ImageFilter')
-attrNameList.remove('ChangeInformationImageFilter')
-# attrNameList.remove('GeodesicActiveContourLevelSetImageFilter')
-attrNameList.remove('Image')
-attrNameList.remove('ImageBase')
-attrNameList.remove('ImageFileReader')
-attrNameList.remove('ImageFileWriter')
-attrNameList.remove('ImageRegistrationMethod')
-attrNameList.remove('BSplineDownsampleImageFilter')
-attrNameList.remove('BSplineUpsampleImageFilter')
-attrNameList.remove('CropImageFilter')
-# attrNameList.remove('LevelSetFunction')
-attrNameList.remove('MattesMutualInformationImageToImageMetric')
-attrNameList.remove('MeanReciprocalSquareDifferenceImageToImageMetric')
-attrNameList.remove('MeanSquaresImageToImageMetric')
-attrNameList.remove('MultiResolutionImageRegistrationMethod')
-# attrNameList.remove('MultiResolutionPyramidImageFilter')
-attrNameList.remove('MutualInformationImageToImageMetric')
-attrNameList.remove('NormalizedCorrelationImageToImageMetric')
-# attrNameList.remove('ParallelSparseFieldLevelSetImageFilter')
-# attrNameList.remove('PyBuffer')
-attrNameList.remove('RawImageIO')
-# attrNameList.remove('RecursiveMultiResolutionPyramidImageFilter')
-attrNameList.remove('ResampleImageFilter')
-attrNameList.remove('RescaleIntensityImageFilter')
-# attrNameList.remove('SegmentationLevelSetImageFilter')
-# attrNameList.remove('ShapeDetectionLevelSetImageFilter')
-# attrNameList.remove('SparseFieldFourthOrderLevelSetImageFilter')
-# attrNameList.remove('SparseFieldLevelSetImageFilter')
-attrNameList.remove('SpatialObject')
-attrNameList.remove('SpatialObjectTreeNode')
-attrNameList.remove('WatershedImageFilter')
-attrNameList.remove('TreeNode')
-# attrNameList.remove('ThresholdSegmentationLevelSetImageFilter')
-attrNameList.remove('PointSet')
-attrNameList.remove('ImageConstIterator')
-attrNameList.remove('ImageConstIteratorWithIndex')
-attrNameList.remove('ImageIterator')
-attrNameList.remove('ImageIteratorWithIndex')
-attrNameList.remove('ImageLinearConstIteratorWithIndex')
-attrNameList.remove('ImageLinearIteratorWithIndex')
-attrNameList.remove('ImageRandomConstIteratorWithIndex')
-attrNameList.remove('ImageRandomIteratorWithIndex')
-attrNameList.remove('ImageRandomNonRepeatingConstIteratorWithIndex')
-attrNameList.remove('ImageRandomNonRepeatingIteratorWithIndex')
-attrNameList.remove('ImageRegionConstIterator')
-attrNameList.remove('ImageRegionIterator')
-attrNameList.remove('ImageRegionConstIteratorWithIndex')
-attrNameList.remove('ImageRegionIteratorWithIndex')
-attrNameList.remove('InterpolateImageFilter')
-attrNameList.remove('IsolatedConnectedImageFilter')
-attrNameList.remove('SmartPointer')
+
+
+parser = OptionParser(usage="usage: %prog")
+parser.add_option("--exclude", dest="exclude", default=None, metavar="FILE", help="")
+parser.add_option("-v", "--verbose", dest="verbose", default=0, type="int", help="")
+(options, args) = parser.parse_args()
+
+
+exclude = set()
+if options.exclude :
+  exclude = set(file(options.exclude).read().splitlines())
+
+
+
+attrNameList = sorted(set([i for i in dir(itk) if i[0].isupper() and len(i) > 2]) - set(excludedClasses))
+
+
+unwrappedTypes = set()
 
 
 for name in attrNameList:
     attr = itk.__dict__[name]
-    print "-----------", name, "-----------"
+    log(name, 1)
     if isinstance(attr, itkTemplate) :
 	exploreTpl(attr)
     else :
-	exploreMethods(attr)
+	exploreMethods(attr, attr)
         try :
-	    exploreMethods(cl.New())
+	    exploreMethods(attr.New(), attr)
 	except:
 	    pass
 	try :
-	    exploreMethods(cl())
+	    exploreMethods(attr(), attr)
 	except:
 	    pass
 								

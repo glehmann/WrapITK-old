d601c4d070c35226d4dff1f46c5733357b7f8cc4
Zachary Pincus <zpincus@stanford.edu>
2006-04-05 00:02:59 +0200

    Get rid of ConstantBoundaryCondition problems by getting rid of ConstantBoundaryCondition
    darcs-hash:20060404220259-733a7-1554fd67520f1b3433f179c57eb8e32a4a292971.gz

--- CommonA/wrap_itkImageBoundaryCondition.cmake
+++ CommonA/wrap_itkImageBoundaryCondition.cmake
@@ -3,9 +3,9 @@
 # that use __declspec specifiers, which one part of itkConstantBoundaryCondition.h
 # does on windows. Just pass the stub header which will keep gcc-xml away from the 
 # real header on windows.
-IF(WIN32)
-  WRAP_INCLUDE("itkConstantBoundaryConditionStub.h")
-ENDIF(WIN32)
+# IF(WIN32)
+#   WRAP_INCLUDE("itkConstantBoundaryConditionStub.h")
+# ENDIF(WIN32)
 
 # WRAP_CLASS("itk::ImageBoundaryCondition")
 #   WRAP_IMAGE_FILTER_INT(1)
@@ -33,11 +33,11 @@ WRAP_CLASS("itk::ZeroFluxNeumannBoundaryCondition")
   WRAP_IMAGE_FILTER_RGB(1)
 END_WRAP_CLASS()
 
-WRAP_CLASS("itk::ConstantBoundaryCondition")
-  WRAP_IMAGE_FILTER_INT(1)
-  WRAP_IMAGE_FILTER_SIGN_INT(1)
-  WRAP_IMAGE_FILTER_REAL(1)
-  WRAP_IMAGE_FILTER_VECTOR_REAL(1)
-  WRAP_IMAGE_FILTER_COV_VECTOR_REAL(1)
-  WRAP_IMAGE_FILTER_RGB(1)
-END_WRAP_CLASS()
+# WRAP_CLASS("itk::ConstantBoundaryCondition")
+#   WRAP_IMAGE_FILTER_INT(1)
+#   WRAP_IMAGE_FILTER_SIGN_INT(1)
+#   WRAP_IMAGE_FILTER_REAL(1)
+#   WRAP_IMAGE_FILTER_VECTOR_REAL(1)
+#   WRAP_IMAGE_FILTER_COV_VECTOR_REAL(1)
+#   WRAP_IMAGE_FILTER_RGB(1)
+# END_WRAP_CLASS()

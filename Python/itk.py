fcec810b325aaafee878dd9b616ec3af0d90adf4
Zachary Pincus <zpincus@stanford.edu>
2006-04-22 03:04:08 +0200

    More fixes for LazyLoading
    darcs-hash:20060422010408-733a7-fb0a7c800e2258d39d06955bd489b639aafcf5d7.gz

--- Python/itk.py
+++ Python/itk.py
@@ -1,31 +1,46 @@
 """itk.py : Top-level container module for ITK wrappers."""
 import itkBase, itkConfig, itkLazy, itkTypes, itkExtras, os, sys
 
+thisModule = sys.modules[__name__]
+
 if itkConfig.LazyLoading:
   # If we are loading lazily (on-demand), make a dict mapping the available 
   # classes/functions/etc. (read from the configuration modules) to the 
   # modules they are declared in. Then pass that dict to a LazyITKModule
-  # instance and do some surgery on sys.modules so that the 'itk' module
-  # becomes that new instance instead of what is executed from this file.
+  # instance and (later) do some surgery on sys.modules so that the 'itk' 
+  # module becomes that new instance instead of what is executed from this file.
   lazyAttributes = {}
   for module, data in itkBase.module_data.items():
     templateNames = [ t[0] for t in data['templates'] ]
     lazyAttributes.update( (n, module) for n in templateNames)
-  sys.modules[__name__] = itkLazy.LazyITKModule(__name__, lazyAttributes)
+  if isinstance(thisModule, itkLazy.LazyITKModule):
+    # Handle reload case where we've already done this once.
+    # If we made a new module every time, multiple reload()s would fail
+    # because the identity of sys.modules['itk'] would always be changing.
+    thisModule.__init__(__name__, lazyAttributes)
+    del lazyAttributes
+  else:
+    thisModule = itkLazy.LazyITKModule(__name__, lazyAttributes)
 else: 
-  # Load the modules in the order specified in the known_modules list
-  # for consistency.
+  # We're not lazy-loading. Just load the modules in the order specified in 
+  # the known_modules list for consistency.
   for module in itkBase.known_modules:
-      itkBase.LoadModule(module, globals())
-
-# Now load up the itk module (either this module, or the LazyITKModule
-# instance created above) with the types and extras information.
-thisModule =  sys.modules[__name__]
+    itkBase.LoadModule(module, thisModule.__dict__)
+  
+# Regardless of how it was loaded, fill up the itk module with the ITK types
+# and extras.
 for k, v in itkTypes.__dict__.items():
-  if k != 'itkCType': setattr(thisModule, k, v)
+  if k != 'itkCType' and not k.startswith('_'): 
+    setattr(thisModule, k, v)
 for k, v in itkExtras.__dict__.items():
-  setattr(thisModule, k, v)
+  if not k.startswith('_'): 
+    setattr(thisModule, k, v)
 
-# Clean up the namespace -- only matters if we aren't using lazy loading.
-del configModules, module, thisModule
-del itkBase, itkConfig, itkLazy, itkTypes, itkExtras, os, sys
+if itkConfig.LazyLoading:
+  # this has to be the last step, else python gets confused about itkTypes
+  # and itkExtras above. I'm not sure why...
+  sys.modules[__name__] = thisModule
+else:
+  # do some cleanup
+  del module, thisModule
+  del itkBase, itkConfig, itkLazy, itkTypes, itkExtras, os, sys

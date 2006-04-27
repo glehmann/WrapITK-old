563867a0d412232d223269af8db334276781ff6b
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-04-27 22:13:14 +0200

    import vtk module before returning a vtk object to fix the segfault and deprecate itkvtk
    darcs-hash:20060427201314-2fc9d-7788977ca5a53d1ddceea3ad0202e277f56927a1.gz

--- ExternalProjects/ItkVtkGlue/Wrapping/Python/itkvtk.py
+++ ExternalProjects/ItkVtkGlue/Wrapping/Python/itkvtk.py
@@ -11,3 +11,9 @@ del itk, vtk, ItkVtkGlue, names, name
 
 # also keep ItkVtkGlue members in that namespace
 from ItkVtkGlue import *
+
+# display a deprecation warning
+import sys
+print >> sys.stderr, "Warning: ItkVtkGlue classes are available in itk module, without importing itkvtk."
+print >> sys.stderr, "Warning: itkvtk is no more supported and will be removed soon."
+del sys

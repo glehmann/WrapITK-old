eb7bdd5acf989e6495197ab791337d0ef4b94dd0
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-06-03 14:17:48 +0200

    reenable some level set classes
    darcs-hash:20060603121748-2fc9d-5e1a68679b752ea01e11872c552752c7a3fcd022.gz

--- Modules/LevelSet/wrap_itkSegmentationLevelSetImageFilter.cmake
+++ Modules/LevelSet/wrap_itkSegmentationLevelSetImageFilter.cmake
@@ -1,11 +1,12 @@
+
+WRAP_INCLUDE("itkShapeDetectionLevelSetImageFilter.h")
+
 WRAP_CLASS("itk::SegmentationLevelSetImageFilter" POINTER)
 
-# cableswig segfaults while processing this wrapper
-
-#   FOREACH(d ${WRAP_ITK_DIMS})
-#     FOREACH(t ${WRAP_ITK_REAL})
-#       WRAP_TEMPLATE("${ITKM_I${t}${d}}${ITKM_I${t}${d}}${ITKM_${t}}" "${ITKT_I${t}${d}},${ITKT_I${t}${d}},${ITKT_${t}}")
-#     ENDFOREACH(t)
-#   ENDFOREACH(d)
+  FOREACH(d ${WRAP_ITK_DIMS})
+    FOREACH(t ${WRAP_ITK_REAL})
+      WRAP_TEMPLATE("${ITKM_I${t}${d}}${ITKM_I${t}${d}}${ITKM_${t}}" "${ITKT_I${t}${d}},${ITKT_I${t}${d}},${ITKT_${t}}")
+    ENDFOREACH(t)
+  ENDFOREACH(d)
 
 END_WRAP_CLASS()

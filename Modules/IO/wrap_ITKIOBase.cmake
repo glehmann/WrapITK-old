2bdb28dae1a676ec4458487d1ca84fdc88429917
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-06-13 21:47:52 +0200

    wrap all IO, IOFactory and SeriesFileNames classes
    darcs-hash:20060613194752-2fc9d-c599dd255384c0b8b1ae8a77bc7af3c135c6bbb8.gz

--- Modules/IO/wrap_ITKIOBase.cmake
+++ Modules/IO/wrap_ITKIOBase.cmake
@@ -4,15 +4,45 @@ WRAP_CLASS("itk::RawImageIO" POINTER)
   ENDFOREACH(d)
 END_WRAP_CLASS()
 
+
+# non templated IO classes and factories
 WRAP_NON_TEMPLATE_CLASS("itk::ImageIOBase" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::PNGImageIO" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::MetaImageIO" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::DicomImageIO" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::PNGImageIOFactory" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::MetaImageIOFactory" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::DicomImageIOFactory" POINTER)
+WRAP_NON_TEMPLATE_CLASS("itk::IPLCommonImageIO" POINTER)
+
+SET(io_classes
+  AnalyzeImageIO
+  BioRadImageIO
+  BMPImageIO
+  Brains2MaskImageIO
+  DICOMImageIO2
+  DicomImageIO
+  GDCMImageIO
+  GE4ImageIO
+  GE5ImageIO
+  GEAdwImageIO
+  GiplImageIO
+  JPEGImageIO
+  LSMImageIO
+  MetaImageIO
+  NiftiImageIO
+  NrrdImageIO
+  PNGImageIO
+  SiemensVisionImageIO
+  StimulateImageIO
+  TIFFImageIO
+  VTKImageIO
+)
+
+FOREACH(c ${io_classes})
+  WRAP_NON_TEMPLATE_CLASS("itk::${c}" POINTER)
+  WRAP_NON_TEMPLATE_CLASS("itk::${c}Factory" POINTER)
+ENDFOREACH(c)
+
+
+# *SeriesFileNames
+WRAP_NON_TEMPLATE_CLASS("itk::ArchetypeSeriesFileNames" POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::DICOMSeriesFileNames" POINTER)
+WRAP_NON_TEMPLATE_CLASS("itk::GDCMSeriesFileNames" POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::NumericSeriesFileNames" POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::RegularExpressionSeriesFileNames" POINTER)
-WRAP_NON_TEMPLATE_CLASS("itk::GDCMImageIO" POINTER)
 

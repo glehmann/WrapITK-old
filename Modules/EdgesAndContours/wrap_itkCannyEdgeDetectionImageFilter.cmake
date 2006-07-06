74097d9ae8cc08eccf14a3b9b412152c6d862f4e
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-07-06 19:04:55 +0200

    reenable classes broken by typemaps
    darcs-hash:20060706170455-2fc9d-837b74cfd12282f315c55f635262b824958478e2.gz

--- Modules/EdgesAndContours/wrap_itkCannyEdgeDetectionImageFilter.cmake
+++ Modules/EdgesAndContours/wrap_itkCannyEdgeDetectionImageFilter.cmake
@@ -1,7 +1,4 @@
-# again that problem with python and FixedArray typemap
-# this class can't be used
-
-# WRAP_CLASS("itk::CannyEdgeDetectionImageFilter" POINTER)
-#  WRAP_IMAGE_FILTER_REAL(2 2+)
-# END_WRAP_CLASS()
+WRAP_CLASS("itk::CannyEdgeDetectionImageFilter" POINTER)
+  WRAP_IMAGE_FILTER_REAL(2 2+)
+END_WRAP_CLASS()
 
--- Modules/Filtering/wrap_itkDiscreteGaussianImageFilter.cmake
+++ Modules/Filtering/wrap_itkDiscreteGaussianImageFilter.cmake
@@ -1,9 +1,5 @@
-# swig generate bad code for python with that filter for an unknown
-# it seems that it is a bad interaction with FixedArray typemap
-# we can't enable it for now
-
-# WRAP_CLASS("itk::DiscreteGaussianImageFilter" POINTER)
-#   WRAP_IMAGE_FILTER_USIGN_INT(2)
-#   WRAP_IMAGE_FILTER_SIGN_INT(2)
-#   WRAP_IMAGE_FILTER_REAL(2)
-# END_WRAP_CLASS()
+WRAP_CLASS("itk::DiscreteGaussianImageFilter" POINTER)
+  WRAP_IMAGE_FILTER_USIGN_INT(2)
+  WRAP_IMAGE_FILTER_SIGN_INT(2)
+  WRAP_IMAGE_FILTER_REAL(2)
+END_WRAP_CLASS()

d2be070451c0701d1f454bca45dbdd027a712c8c
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-07-27 17:07:38 +0200

    disable MetadataDictionary too fix build problem on some systems
    darcs-hash:20060727150738-2fc9d-71c3b7bf66e75aff4a0422c22bfbb9c8fd730845.gz

--- Modules/Base/wrap_ITKCommonBase.cmake
+++ Modules/Base/wrap_ITKCommonBase.cmake
@@ -12,5 +12,10 @@ WRAP_NON_TEMPLATE_CLASS("itk::Version"            POINTER)
 WRAP_NON_TEMPLATE_CLASS("itk::TimeStamp")
 WRAP_NON_TEMPLATE_CLASS("itk::Indent")
 WRAP_NON_TEMPLATE_CLASS("itk::StringStream")
-WRAP_NON_TEMPLATE_CLASS("itk::MetaDataDictionary")
+
+#
+# disabled to fix too long name on some systems with java
+# see http://public.kitware.com/pipermail/insight-users/2006-July/018785.html
+#
+# WRAP_NON_TEMPLATE_CLASS("itk::MetaDataDictionary")
 
